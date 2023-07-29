93ec4e5c2c5bf3e568d1113cb8469a89ea489ac7
baron <buaisaac17@gmail.com>
2023-07-29 18:10:56 +0300

    writen the calculator function for the 4 operations, addition, subtraction, divisionand multiplication

---
+++ scripts.js
@@ -0,0 +1,42 @@
+console.log("baron");
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+function calculator(num1, operator, num2){
+    if (operator === '+'){
+        //add
+        function add(num1, num2){
+        const sum = num1 + num2;
+        return sum;
+        };
+        return add(num1, num2);
+        //console.log(add(3,7));
+    } else if (operator === '-'){
+        //subtract
+        function subtract(num1, num2){
+        const difference = num1 - num2;
+        return difference;
+        };
+        return subtract(num1, num2);
+        //console.log(subtract(10, 3));
+    } else if (operator === '/'){
+        //division
+        function division(num1, num2){
+        const divide = num1 / num2;
+        return divide;
+        };
+        return division(num1, num2);
+        //console.log(division(10, 5));
+    } else if (operator === '*'){
+        //miltiply
+        function multiply(num1, num2){
+        const mul = num1 * num2;
+        return mul;
+        };
+        return multiply(num1, num2);
+        //console.log(multiply(2,2));
+    };
+};
+//result 
+console.log(calculator(2,'/', 0));
